4bf3d4901884ce3dba7b03462b54424473acf826
Zane Dunn <60718015+dunndeft@users.noreply.github.com>
2022-07-14 18:30:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+		//Build a Console Creature
+    Console.WriteLine("     .-*-. ");
+    Console.WriteLine("   --|...|--");
+    Console.WriteLine("     .---. ");
+    Console.WriteLine("     (o o) ");
+    Console.WriteLine("   /_| O |_/ ");
+    Console.WriteLine("     |   | ");
+    Console.WriteLine("    '~~~~~'");
+    Console.WriteLine("");
+    Console.WriteLine("- Ghost with a hat");
+    Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ _");
+    Console.WriteLine("");
+    Console.WriteLine("c_     ___*-");
+    Console.WriteLine("　 \\__|____|__");
+    Console.WriteLine("　　 \"( o_o)");
+    Console.WriteLine("　　　 > ~  >");
+    Console.WriteLine("　　　/ 　 / \\");
+    Console.WriteLine("　　 /　　/　 \\");
+    Console.WriteLine("　　 \"　 )　　c/");
+    Console.WriteLine("　　/　 /");
+    Console.WriteLine("　 /　/|");
+    Console.WriteLine("　(　( \"");
+    Console.WriteLine("　|　|  \"");
+    Console.WriteLine("　| / \"  )");
+    Console.WriteLine("　| |　) |");
+    Console.WriteLine(" /  )(_/");
+    Console.WriteLine("(_ /");
+    Console.WriteLine("");
+  	Console.WriteLine("– Dancing Man");
+	}
+}
